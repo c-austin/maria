a7e1827277ca29acdd47b0d34bef9fa09bdd1139
Cory Austin <cory.austin@ryerson.ca>
2023-09-27 09:33:51 -0700

    v1

---
+++ index.js
@@ -0,0 +1,15 @@
+var model = document.getElementById('model');
+var loadScreen = document.getElementById('loading-modal');
+var content = document.getElementById('content');
+
+model.addEventListener('load', function () {
+    var button = model.shadowRoot.querySelector("#default-ar-button");
+    loadScreen.style.opacity = '0'
+    setTimeout(() => { loadScreen.style.display = 'none'; }, 3000);
+    if (button.offsetParent !== null) {
+        content.style.transition = 'opacity 0s';
+        content.style.transitionDelay = '5s';
+        setTimeout(() => button.click(), 2000);
+    }
+    content.style.opacity = '1'
+});
